011f8a8fb9705aca4af7f3421d54cbba5ad63557
alreicha <areichard@haverford.edu>
2020-07-16 09:35:21 -0400

    heroku change

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Gourdon-Hamsey Burger ', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Poblano Picasso Burger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("'Don't You Four Cheddar 'Bout Me Burger'", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Paranormal Pepper Jack-tivity Burger ', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Thomas Elphinstone Hambledurger with Manning Coleslaw', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Gourdon-Hamsey Burger ', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Poblano Picasso Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("'Don't You Four Cheddar 'Bout Me Burger'", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Paranormal Pepper Jack-tivity Burger ', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Thomas Elphinstone Hambledurger with Manning Coleslaw', false);
